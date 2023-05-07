a1745e6b556f46ae8edd6cad5aba58e1b22881f2
Anett K <anett.kollin@tktk.ee>
2023-05-07 14:22:45 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment to my code
+
+// rakendus palub kasutajal sisestada oma nimi 
+//rakendus tervitab kasutajat nimepidi 
+Console.WriteLine("Enter your name:"); //Output
+
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
